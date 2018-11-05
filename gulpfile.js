e5cb5d5cc79989d0164fe135d0d95b50bb14a440
xuyy121840 <xuyy163.com>
2018-11-05 09:06:08 +0800

    服务完成

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+var gulp = require('gulp');
+var server = require('gulp-webserver');
+var sass = require('gulp-sass');
+var fs = require('fs');
+var path = require('path');
+var url = require('url');
+
+
+gulp.task('server', function() {
+    return gulp.src('src')
+        .pipe(server({
+            port: 9090,
+            middleware: function(req, res, next) {
+                var pathname = url.parse(req.url).pathname;
+                if (req.url === '/favicon.ico') {
+                    res.end('');
+                    return false;
+                }
+                if (pathname === '/api/list') {
+
+                } else {
+                    pathname = pathname === '/' ? 'index.html' : pathname;
+                    res.end(fs.readFileSync(path.join(__dirname, 'src', pathname)))
+                }
+            }
+        }))
+});
+
+gulp.task('sass', function() {
+    return gulp.src('./src/scss/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./src/css'))
+})
+
+gulp.task('watch', function() {
+    return gulp.watch('./src/scss/*.scss', gulp.series('sass'))
+})
+
+gulp.task('dev', gulp.series('sass', 'server', 'watch'))
